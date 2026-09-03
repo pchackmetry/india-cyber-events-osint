dcce9e4bc1e1bec3b388970ef71de98c68f483b1
Katakam Likith Kumar <158552200+pchackmetry@users.noreply.github.com>
2026-09-03 19:33:09 +0530

    Add bot_test.py to interact with Telegram API

---
+++ bot_test.py
@@ -0,0 +1,10 @@
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+
+url = f"https://api.telegram.org/bot{TOKEN}/getMe"
+
+response = requests.get(url, timeout=20)
+
+print(response.json())
